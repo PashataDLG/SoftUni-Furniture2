45d7f70b3392b2bfe9ed7b9a24d543c9158b8c4c
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2022-07-02 16:00:44 +0300

    DB model created

--- server/src/controllers/furnitureController.js
+++ server/src/controllers/furnitureController.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
-router.get('/', function (req, res){
+router.get('/', function (req, res) {
     res.json([]);
 });
+
+// router.post('/', function (req, res){
+    
+// }); 
+
 
 module.exports = router;
---
+++ server/src/models/Furniture.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const furnitureSchema = new mongoose.Schema({
+    make: {
+      type: String,
+      minlength: [4, 'The make field must contains at least 4 characters']
+    },
+    model: {
+      type: String,
+      minlength: [4, 'The model field must contains at least 4 characters']
+    },
+    year: {
+        type: Number,
+        min: [1950, 'Year must be between 1950 and 2050'],
+        max: [2050, 'Year must be between 1950 and 2050'],
+    },
+    description: {
+        type: String,
+        minlength: [11, 'The description must be at least 11 characters long'], 
+    },
+    price: {
+        type: Number,
+        min: [0.01, 'The price must be a positive number'],
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    material: {
+        type: String,
+        required: false,
+    },
+    ownerId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }
+});
+
+const Furniture = mongoose.model('Furniture', furnitureSchema);
+
+module.exports = Furniture;
