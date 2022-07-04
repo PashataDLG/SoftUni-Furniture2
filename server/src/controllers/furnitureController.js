f44ceccd6b798fd22a03b3bfe947c095acaa4fb5
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2022-07-04 11:32:24 +0300

    Create Functionallity added

--- server/src/controllers/furnitureController.js
+++ server/src/controllers/furnitureController.js
@@ -3,9 +3,21 @@ const router = require('express').Router();
 const furnitureService = require('../services/furnitureService');
 
 router.get('/', async function (req, res) {
+    console.log(req.query.where);
     const furniture = await furnitureService.getAll();
 
     res.json(furniture);
+});
+
+router.post('/', async function (req, res) {
+    try {
+        const result = await furnitureService.createFurniture(req.body);
+        
+        res.json(result);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ message: 'Request error'});
+    }
 });
 
 
